5f364be7be307c2acfb769c6d4cd73eb54c42937
rozhkovs <rozhkovse1@gmail.com>
2024-07-17 17:24:57 +0700

    Configure prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "arrowParens": "always",
+  "bracketSameLine": true,
+  "bracketSpacing": true,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "endOfLine": "lf",
+  "tabWidth": 2,
+  "useTabs": false
+}
